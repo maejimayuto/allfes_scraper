0c634b66c54bc396f9ab5df243d6a2e62d2c79d6
maejimayuto <maejimayuto@gmail.com>
2022-04-12 13:45:58 +0900

    remove: i ref this qiita
    https://qiita.com/Chanmoro/items/9a3c86bb465c1cce738a

--- sample.py
+++ sample.py
@@ -16,20 +16,5 @@ driver.get('https://carpe.dev')
 print(driver.current_url)
 driver.save_screenshot('search_results.png')
 
-from selenium.webdriver.common.action_chains import ActionChains
-actions = ActionChains(driver)
-
-# MOVE TO TOP_LEFT (`move_to_element` will guide you to the CENTER of the element)
-whole_page = driver.find_element_by_tag_name("html")
-actions.move_to_element_with_offset(whole_page, 0, 0)
-
-# MOVE TO DESIRED POSITION THEN CLICK
-for x in range(80):
-    print(x)
-    actions.move_by_offset(x, 18)
-    actions.click()
-    actions.perform()
-    driver.save_screenshot(f"search_results-{x}.png")
-
 # ブラウザを終了する
 driver.quit()
